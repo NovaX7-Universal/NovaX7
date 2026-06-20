a758fb0eb2f40e952f5b20fcfa0d346aea5d2284
NovaX7-Universal <blattgold1985@gmail.com>
2026-06-20 22:41:21 +0200

    Nova update v7.1.6

--- NovaX7_7_1_0.py
+++ NovaX7_7_1_0.py
@@ -1,4 +1,4 @@
-# Version: 7.1.5
+# Version: 7.1.6
 import sys as _sys
 import os as _os
 
@@ -5506,7 +5506,7 @@ import hashlib as _hashlib
 import urllib.request as _urllib_req
 
 _NOVA_CONFIG = os.path.join(os.path.dirname(os.path.abspath(__file__)), "nova_config.json")
-_NOVA_VERSION = "7.1.5"
+_NOVA_VERSION = "7.1.6"
 _NOVA_UPDATE_URL = (
     "https://raw.githubusercontent.com/NovaX7-Universal/NovaX7/main/NovaX7_7_1_0.py"
 )
